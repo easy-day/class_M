1e430c2b051e4423f5f7052faa714209bd8d63d2
15박지훈 <15jihun@LOCS-2.local>
2020-09-24 01:30:32 +0900

    initial commits

---
+++ djangoProject1/test123/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Test123Config(AppConfig):
+    name = 'test123'
---
+++ pythonProject/String.py
@@ -0,0 +1,4 @@
+str1 = "Hello"
+str2 = "goorm: !@#$%^&*()\|/"
+str3 = "3"
+print(str1, str2, str1 + str3)
